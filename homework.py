91935b532e6bedd454e8a30cd45f1a914b9fbc0b
Aleksandr Riabinskii <olexander.rabota@yandex.ru>
2023-11-05 08:49:11 +0300

    Git push and Git pull check

--- homework.py
+++ homework.py
@@ -1,2 +1,45 @@
-# Testing git push
+# Колькулятор для подсчета денег и колорий
+
+class Calculator:
+    """Class saves new record, counts spend of ... today, counts spend of ... for seven days"""
+    def __init__(self, limit) -> None:
+        self.limit = limit
+
+    records = []
+
+    def add_record(record):
+        """Method appends new record """
+        records.append(record)
+
+
+    def get_today_stats():
+        
+    def get_week_stats():
+
+
+
+
+class Records:
+    """Class records amount cash or callories, current date, comment """
+    def __init__(self, amount, date, comment) -> None:
+        self.amount = amount
+        self.date = date
+        self.comment = comment
+        
+        
+
+
+
+
+class CaloriesCalculator(Calculator):
+    
+
+
+
+class CashCalculator(Calculator):
+
+
+
+
+
 
