cc252917b1316318dc7ac54e63a276b25a2be2ad
Aleksandr Riabinskii <olexander.rabota@yandex.ru>
2023-11-08 06:24:41 +0300

    рабочий проект

--- homework.py
+++ homework.py
@@ -1,92 +1,102 @@
 # Колькулятор для подсчета денег и колорий
 import datetime as dt
 date_format = '%d.%m.%Y'
-now = dt.datetime.now().date()
 
-class Records:
-    """Class records amount cash in rubles  or callories in kcal, current date, comment and sends data in calculator"""
-    def __init__(self, amount, date, comment) -> None:
+class Record: # Этот класс работает пиздато, но пока хуевинько !!!!!
+    """Class records amount cash in rubles  or callories in kcal, current date,
+       comment and sends data in calculator."""
+    def __init__(self, amount, comment, date=dt.datetime.now().date()):
         self.amount = amount
-        self.date = dt.datetime.strptime(date, date_format)
+        self.date = date
         self.comment = comment
 
 
 class Calculator:
-    """Class saves new record, counts spend of ... today, counts spend of ... for seven days"""
+    """Class saves new record, counts spend of ... today, counts spend of ... 
+    for seven days."""
     def __init__(self, limit) -> None:
         self.limit = limit
+        self.records = []  # здесь лежат все объекты класса records, 
+        # то есть все записи
 
-    records = [] # здесь лежат все объекты класса records, то есть все записи
-    
+        self.records_day = []  # здесь лежат все значчения summ
+        self.records_day_only = []  # здесь лежат все значения amount 
+        # для настоящего дня
+        self.last_of_seven_days = []
+        
+
     def add_record(self, record):
-        """Method appends new record """
+        """Method appends new record."""
         self.records.append(record)
+        print(record.date)
 
-    records_day = [] # здесь лежат все значчения summ 
     def get_today_stats(self):
-        """counts cost per day"""
-        records_day_only = [] # здесь лежат все значения amount для настоящего дня
+        """counts cost per day."""
+        now = dt.datetime.now().date()
+        summ = 0
+
         for i in self.records:
             if i.date == now:
-                records_day_only.append(i.amount)
-        summ = 0
-        for i in records_day_only:
-            summ += i
+                summ += i.amount
         return summ
 
-        
     def get_week_stats(self):
-        """counts cost per week"""
-        last_of_seven_days = []
-        start = now - dt.timedelta(days=7)
-        for i in range(7):
-            last_of_seven_days.append(now - dt.timedelta(i))
+        """counts cost per week."""
         summ = 0
+        now = dt.datetime.now().date()
+        tomorow = now + dt.timedelta(days=1)
+        week_ago = now - dt.timedelta(days=7)
+
         for i in self.records:
-            if i.date in last_of_seven_days:
+            if week_ago<i.date<tomorow:
                 summ += i.amount
         return summ
     
-
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         remained = self.limit - super().get_today_stats()
         if remained > 0:
-            return f'Сегодня можно съесть что-нибудь еще, но c общей калорийностью не более {remained} Ккал'
+            return (f'Сегодня можно съесть что-нибудь еще, но c общей'
+                    f'калорийностью не более {remained} Ккал.')
         else:
-            return f'Хватит есть!'
+            return 'Хватит есть!'
 
 
-    
-    
 class CashCalculator(Calculator):
-    USD_RATE = 93.04
-    EURO_RATE = 99.01
-    def get_today_cash_remained(self, curruncy):
-        if curruncy  == 'rub':
-            return super().get_week_stats()
-        elif currency =='usd':
-            return super().get_week_stats()/USD_RATE
+
+    def get_today_cash_remained(self, currency):
+        USD_RATE = 93.04
+        EURO_RATE = 99.01
+        result = super().get_today_stats()
+        total = self.limit - result
+        
+        if currency == 'rub':
+            ret = f'{total}, rub'
+        elif currency == 'usd':
+            ret = f'{abs(round(total/ USD_RATE, 2))}, usd'
         else:
-            return super().get_today_stats()/EURO_RATE
+            ret = f'{abs(round(total/ EURO_RATE, 2))}, euro'
+
+        if total > 0:
+            return (f'На сегодня осталось {ret}.')
+        else:
+            return (f'Денег нет! Держись: твой долг - {ret}.')
         
 
 
-
-
 cash_calculator = CashCalculator(1000)
+cash_calculator.add_record(Record(amount=105, comment='Печенье'))
+cash_calculator.add_record(Record(amount=1505, comment='Стоянка'))
+# print(cash_calculator.get_today_stats())
+# print(dt.datetime.now().date())
+print(cash_calculator.get_today_cash_remained('usd'))
+# print(cash_calculator.get_week_stats())
 
-cash_calculator.add_record(Records(amount=105, comment= 'Печенье', date = '5.11.2023'))
-
-
-print(cash_calculator.get_today_cash_remained('rub'))
-
-print(cash_calculator.get_today_stats)
-
-print(cash_calculator.amount)
-
-
+#calories_calculator = CaloriesCalculator(2000)
+#calories_calculator.add_record(Record(amount=205, comment='Вафли'))
+#calories_calculator.add_record(Record(amount=305, comment='Булочка'))
+#print(calories_calculator.get_calories_remained())
 
 
 
